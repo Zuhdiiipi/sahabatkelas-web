4cecedf661b2c9c408a65e4ab77d02489234b29d
Armawan02 <armawanome47@gmail.com>
2026-07-18 19:31:13 +0800

    up

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selamat Datang - SahabatKelas</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         .hero-grid {
             background-image:
